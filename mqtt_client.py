9e6f10c61db0949c3d375b9e9c7ebfa0f8374b18
Garyuu <shuhanrpg@gmail.com>
2016-08-04 22:39:35 +0800

    A little start...

---
+++ mqtt_client.py
@@ -0,0 +1,10 @@
+import configuration
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(CONF_TOPIC)
+
+def mqtt_create(host, topic):
+    client = mqtt.Client()
+    
+
