e0eb6630a65555fd68fe0cff7a1ceb802e4ac1af
Garyuu <shuhanrpg@gmail.com>
2016-08-10 01:06:55 +0800

    Publish is ok

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,10 +1,21 @@
-import configuration
+import configuration as config
 import paho.mqtt.client as mqtt
 
+MQTT_Config = config.ConfigSectionMap('MQTT')
+
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(CONF_TOPIC)
+    client.subscribe(MQTT_Config['topic'])
 
-def mqtt_create(host, topic):
+#def on_publish(client, userdata, mid):
+#    print("published")
+
+def create(on_message):
     client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    #client.on_publish = on_publish
+    client.connect(MQTT_Config['host'])
+    return client
 
-
+def publish(client, message):
+    client.publish(MQTT_Config['topic'], message)
