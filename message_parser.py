a17f9cb62e480a5dc7ec57207275c884c968e966
Garyuu <shuhanrpg@gmail.com>
2016-08-15 02:32:17 +0800

    Parser completed
    In parser:
     Added a sample in header.
     Parse function added.

--- message_parser.py
+++ message_parser.py
@@ -4,6 +4,15 @@ Date:   2016/8/15
 Name:   message_parser
 Descr.: To parse a message from mqtt server.
         The message will be parsed to a command.
+        Message format will be like:
+            10A: X,5,4,2,1,m set2 point:22
 '''
-def get_command(message):
-    
+def parse(message):
+    raw = message.split()
+    data = {
+        'player'    : raw[0][:-1],
+        'score'     : raw[1].split(','),
+        'wave'      : raw[2][4:],
+        'total'     : raw[3][6:],
+    }
+    return data
