febb60ad998ea590aeefadce8e1d532311ed705b
Garyuu <shuhanrpg@gmail.com>
2016-08-14 22:04:39 +0800

    Simple console is completed.

--- console.py
+++ console.py
@@ -1,1 +1,62 @@
 import controller as Controller
+import cmd
+import sys
+
+class ScoringConsole(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '-->'
+
+    def do_exit(self, argstr):
+        sys.exit(1)
+
+    def do_hello(self, argstr):
+        args = argstr.split()
+        if len(args) == 0:
+            Controller.hello('all')
+        elif len(args) > 1:
+            print('Too mant arguments.')
+        elif not isnumeric(args[0]):
+            print('The arguments should be numeric.')
+        else:
+            Controller.hello(args[0])
+
+    def do_status(self, argstr):
+        args = argstr.split()
+        if len(args) > 0:
+            print('The command should not have any arguments.')
+        else:
+            Controller.displat_status()
+
+    def do_assign(self, argstr):
+        args = argstr.split()
+        if len(args) < 2:
+            print('This command should have at least 2 arguments.')
+        elif len(args) > 2:
+            print('Too mant arguments.')
+        elif not (isnumeric(args[0]) and isnumeric(args[1])):
+            print('The arguments should be numeric.')
+        else:
+            Controller.assign(int(args[0]), int(args[1]))
+
+    def do_set(self, argstr):
+        args = argstr.split()
+        if len(args) < 2:
+            print('This command should have at least 2 arguments.')
+        else:
+            Controller.set(int(args[0]), args[1:len(args)])
+
+    def do_setrule(self, argstr):
+        args = argstr.split()
+        if len(args) == 0:
+            print('This command should have 1 arguments.')
+        elif len(args) > 1:
+            print('Too many arguments.')
+        else:
+            Controller.setrule(args[0])
+
+def console():
+    c = ScoringConsole()
+    c.cmdloop()
+
+console()
--- controller.py
+++ controller.py
@@ -1,8 +1,2 @@
-import configuration as Config
-import mqtt_client as MQTT
-import message_parser as Parser
-import message_generator as Generator
 import command_processor as Processor
-import status as Status
-
 
