0de44c5290dace58fe2d89844b42fac8f107499d
garyuu <shuhanrpg@gmail.com>
2016-10-16 02:14:54 +0800

    Construct match maker

--- match_maker.py
+++ match_maker.py
@@ -3,5 +3,23 @@ Author: Garyuu
 Date:   2016/8/30
 Name:   match_maker
 Descr.: Deal with match making. Also the controller of substage.
+        Save section infomation.
 '''
-import dbaccesss
+import enum
+import dbaccess
+
+class MatchMaker:
+    def make(self, player_data, q_to_d=False):
+        for group in player_data.groups:
+            if q_to_d:
+                self.group_make_qtod(group)
+            else:
+                self.group_make_dtod(group)
+
+    def group_make_qtod(self, group):
+        # TODO MM from qualifying to dual match
+        pass
+
+    def group_make_dtod(self, group):
+        # TODO MM to eliminate players from dual match
+        pass
