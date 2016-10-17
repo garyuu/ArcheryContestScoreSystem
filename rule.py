49f56ed4b3df82eb513909a15d44acc5cec85c00
garyuu <shuhanrpg@gmail.com>
2016-10-18 00:22:39 +0800

    A class to load rule

---
+++ rule.py
@@ -0,0 +1,32 @@
+'''
+Author: Garyuu
+Date:   2016/10/17
+Name:   rule
+Descr.: Load rule of the stage assigned.
+'''
+
+from configuration import SectionConfig
+
+class Rule:
+    def __init__(self, stage, rulename):
+        self.game_mode = ''
+        self.scoring_type = ''
+        self.total_waves = 0
+        self.shots_per_wave = 0
+        self.win_point = 0
+        self.lose_point = 0
+        self.draw_point = 0
+        self.goal_point = 0
+        self.load_stage_rule(stage, rulename)
+
+    def load_stage_rule(self, stage, rulename)
+        config = SectionConfig("rules/"+rulename, stage)
+        self.game_mode = config['mode']
+        self.scoring_type = config['scoring']
+        self.total_waves = config['waves']
+        self.shots_per_wave = config['shots']
+        self.win_point = config['win']
+        self.lose_point = config['lose']
+        self.draw_point = config['draw']
+        self.goal_point = config['goal']
+
