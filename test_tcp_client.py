a9be19a3a1deda1af57b928ba6361056d70d574c
garyuu <shuhanrpg@gmail.com>
2017-02-27 18:51:23 +0800

    Test files

---
+++ test_tcp_client.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('', 20000))
+sock.send(bytes("Hello I'm Client.\r\n", 'UTF-8'))
+print(sock.recv(1024))
+sock.send(bytes("I Have no dick.\r\n", 'UTF-8'))
+print(sock.recv(1024))
+#sock.close()
+while True:
+    time.sleep(5)
+    print('5 seconds wait')
